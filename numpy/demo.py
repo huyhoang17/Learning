67e123b356434ca32ff3de14758429e1eb05200f
huyhoang17 <hoangphan0710@gmail.com>
2017-03-16 00:27:13 +0700

    add numpy

---
+++ numpy/demo.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# grid of values
+# same type
+# indexed by a tuple of nonnagetive integers
+# rank: the number of the array
+# shape: a tuple of integers giving the size of the array
+#        along each dimension
+
+
+# a = np.array([1, 2, 3])
+# print(type(a))
+# print(a.shape)  # return a tuple
+# print(a[0], a[1], a[2])
+# a[0] = 5
+# print(a)
+
+# b = np.array([[1, 2, 3], [4, 5, 6]])
+# print('b = ', b)
+# print(b.shape)
+# print(b[0, 0], b[0, 1], b[1, 0])
+
+a = np.zeros((2, 2))
+print(a)
